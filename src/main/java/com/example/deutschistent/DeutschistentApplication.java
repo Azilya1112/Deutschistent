1babe97a1d2416641e830f315dd13612874c2a74
Azilia Adylgazieva <Azilia.Adylgazieva.o3k@fh-zwickau.de>
2024-10-25 12:59:29 +0200

    Initial commit

---
+++ src/main/java/com/example/deutschistent/DeutschistentApplication.java
@@ -0,0 +1,13 @@
+package com.example.deutschistent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeutschistentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeutschistentApplication.class, args);
+	}
+
+}
