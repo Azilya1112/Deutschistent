d1ba0cab829e089a0340eac892bc70e86d7a7c2a
ais24nxh <Aiana.Sagynbekova.nxh@fh-zwickau.de>
2024-10-25 14:24:04 +0200

    feat: add entities, enums

--- src/main/java/com/example/deutschistent/entity/Document.java
+++ src/main/java/com/example/deutschistent/entity/Document.java
@@ -1,4 +1,36 @@
 package com.example.deutschistent.entity;
 
+import com.example.deutschistent.enums.Type;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name="Document")
 public class Document {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    Long id;
+
+    String title;
+
+    String uploaderName;
+
+    Long fileSize;
+
+    LocalDateTime uploadDate;
+
+    Type type;
+
 }
--- src/main/java/com/example/deutschistent/entity/User.java
+++ src/main/java/com/example/deutschistent/entity/User.java
@@ -1,4 +1,30 @@
 package com.example.deutschistent.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name="User")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String firstName;
+
+    String lastName;
+
+    String email;
+
+    String password;
+
 }
